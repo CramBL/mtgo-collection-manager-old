1feb5e1d9a9e8213640977d570988e3f51d462c9
Marc <marcbkonig@gmail.com>
2023-08-13 11:07:19 +0900

    add getter cmd for goat price history data

---
+++ go-backend/cmd/mtgogetter/download/goat-price-hist.go
@@ -0,0 +1,20 @@
+package download
+
+import (
+	"github.com/CramBL/mtgo-collection-manager/go-backend/pkg/mtgogetter"
+	"github.com/spf13/cobra"
+)
+
+const GoatbotsPriceHistoryUrl string = "https://www.goatbots.com/download/price-history.zip"
+
+var DownloadGoatbotsPriceHistoryCmd = &cobra.Command{
+	Use:     "goatbots-price-history",
+	Aliases: []string{"goat-price-hist", "goat-ph", "gph"},
+	Short:   "Download the price history for cards on Goatbots.com",
+	Args:    cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		dl_buf := mtgogetter.DownloadToBuf(GoatbotsPriceHistoryUrl)
+		reader := mtgogetter.UnzipFromBuf(dl_buf)
+		mtgogetter.FirstFileFromZipToDisk("price-history.json", reader)
+	},
+}
