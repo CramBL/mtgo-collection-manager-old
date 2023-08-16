d762faee98d794368cd842f6f737db4247716047
Clang Robot <robot@example.com>
2023-08-16 06:59:33 +0000

    :art: Committing clang-format changes

--- mtgoparser/include/goatbots.hpp
+++ mtgoparser/include/goatbots.hpp
@@ -3,47 +3,48 @@
 #include "glaze/glaze.hpp"
 #include "io.hpp"
 
-#include <optional>
-#include <iostream>
 #include <concepts>
+#include <iostream>
+#include <optional>
 
-namespace goatbots
-{
-   struct CardDefinition
-   {
-      std::string name{};
-      std::string cardset{};
-      std::string rarity{};
-      uint8_t foil{}; // actually boolean but 0/1
-   };
+namespace goatbots {
+struct CardDefinition {
+  std::string name{};
+  std::string cardset{};
+  std::string rarity{};
+  uint8_t foil{}; // actually boolean but 0/1
+};
 
-   using price_hist_map_t = std::unordered_map<std::string, double>;
-   using card_defs_map_t = std::unordered_map<std::string, CardDefinition>;
+using price_hist_map_t = std::unordered_map<std::string, double>;
+using card_defs_map_t = std::unordered_map<std::string, CardDefinition>;
 
-   template<class T>
-   concept goatbots_json = std::disjunction<std::is_same<T, price_hist_map_t>, std::is_same<T, card_defs_map_t>>::value;
+template <class T>
+concept goatbots_json =
+    std::disjunction<std::is_same<T, price_hist_map_t>,
+                     std::is_same<T, card_defs_map_t>>::value;
 
-   template<goatbots_json T>
-   [[nodiscard]] auto ReadJsonMap(std::filesystem::path path_json) -> std::optional<T> {
-      // Instantiate and pre-allocate map
-      T json_map = {};
-      json_map.reserve(80000);
+template <goatbots_json T>
+[[nodiscard]] auto ReadJsonMap(std::filesystem::path path_json)
+    -> std::optional<T> {
+  // Instantiate and pre-allocate map
+  T json_map = {};
+  json_map.reserve(80000);
 
-      // Read file into buffer and decode to populate map
-      if (auto err_code = glz::read_json(json_map, io_util::ReadFile(path_json))) {
-         // Handle error
-         std::string descriptive_error = glz::format_error(err_code, std::string{});
-         std::cout << "ERR=" << err_code << " parsing json: " << descriptive_error << '\n';
-         return std::nullopt;
-      }
+  // Read file into buffer and decode to populate map
+  if (auto err_code = glz::read_json(json_map, io_util::ReadFile(path_json))) {
+    // Handle error
+    std::string descriptive_error = glz::format_error(err_code, std::string{});
+    std::cout << "ERR=" << err_code << " parsing json: " << descriptive_error
+              << '\n';
+    return std::nullopt;
+  }
 
-      return json_map;
-   }
+  return json_map;
 }
+} // namespace goatbots
 
-template <>
-struct glz::meta<goatbots::CardDefinition>
-{
-   using T = goatbots::CardDefinition;
-   static constexpr auto value = object("name", &T::name, "cardset", &T::cardset, "rarity", &T::rarity, "foil", &T::foil);
+template <> struct glz::meta<goatbots::CardDefinition> {
+  using T = goatbots::CardDefinition;
+  static constexpr auto value = object("name", &T::name, "cardset", &T::cardset,
+                                       "rarity", &T::rarity, "foil", &T::foil);
 };
--- mtgoparser/include/io.hpp
+++ mtgoparser/include/io.hpp
@@ -6,20 +6,19 @@
 
 namespace io_util {
 
-[[nodiscard]] auto ReadFile(std::filesystem::path fpath) -> std::string
-{
-    // Open the stream to 'lock' the file.
-    std::ifstream file(fpath, std::ios::in | std::ios::binary);
+[[nodiscard]] auto ReadFile(std::filesystem::path fpath) -> std::string {
+  // Open the stream to 'lock' the file.
+  std::ifstream file(fpath, std::ios::in | std::ios::binary);
 
-    // Obtain the size of the file.
-    const auto fsize = std::filesystem::file_size(fpath);
+  // Obtain the size of the file.
+  const auto fsize = std::filesystem::file_size(fpath);
 
-    // Create a buffer.
-    std::string str_buffer(fsize, '\0');
+  // Create a buffer.
+  std::string str_buffer(fsize, '\0');
 
-    // Read the whole file into the buffer.
-    file.read(str_buffer.data(), fsize);
+  // Read the whole file into the buffer.
+  file.read(str_buffer.data(), fsize);
 
-    return str_buffer;
+  return str_buffer;
 }
-}
+} // namespace io_util
