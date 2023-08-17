051237fed662336e90e14384dd2eb9f2d8412015
Marc <marcbkonig@gmail.com>
2023-08-17 19:07:19 +0900

    add initial trade list xml parsing

--- mtgoparser/include/mtgo.hpp
+++ mtgoparser/include/mtgo.hpp
@@ -4,13 +4,19 @@
 
 #include <rapidxml/rapidxml.hpp>
 #include <rapidxml/rapidxml_utils.hpp>
+#include <spdlog/spdlog.h>
+
 
 namespace mtgo {
 void parse_dek_xml(std::filesystem::path path_xml)
 {
-  auto buf = io_util::ReadFile(path_xml);
+  spdlog::info("Reading from {}", path_xml.string());
+  std::vector<char> buf = io_util::ReadToCharBuf(path_xml);
+  spdlog::info("Got {} bytes", buf.size());
   rapidxml::xml_document<> doc;
-  doc.parse<0>(reinterpret_cast<char *>(&buf));
+  doc.parse<0>(&buf[0]);
+
+  spdlog::info("First node: {}", doc.first_node()->name());
 }
 
 }// namespace mtgo
