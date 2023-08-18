87f869693897767a7a025aeb277e44ff543c7689
Marc <marcbkonig@gmail.com>
2023-08-18 16:40:06 +0800

    add Size member

--- mtgoparser/include/collection.hpp
+++ mtgoparser/include/collection.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "mtgo.hpp"
 
 #include <vector>
@@ -9,9 +8,14 @@ class Collection
 {
 public:
   [[nodiscard]] explicit Collection(std::vector<Card> &&cards) : cards_{ cards } {}
+  [[nodiscard]] constexpr auto Size() const noexcept -> std::size_t;
 
 private:
   // TODO: Add timestamp
   std::vector<Card> cards_;
 };
+
+constexpr auto mtgo::Collection::Size() const noexcept -> std::size_t { return cards_.size(); }
+
+
 }// namespace mtgo
