830e22d5d9a3541f82c29184e2d28f0f1af45ac1
Marc <marcbkonig@gmail.com>
2023-08-20 17:06:00 +0200

    remove commented code

--- mtgoparser/include/mtgoparser/mtgo/card.hpp
+++ mtgoparser/include/mtgoparser/mtgo/card.hpp
@@ -9,31 +9,26 @@ struct Card
   [[nodiscard]] explicit constexpr Card(const char *id,
     const char *quantity,
     const char *name,
-    // const char *annotation,
     const char *set = "",
     const char *rarity = "",
     bool foil = false,
     double price = 0)
     : id_{ id }, name_{ name }, set_{ set }, quantity_{ quantity }, rarity_{ rarity }, foil_{ foil }, price_{ price }
-  //, annotation_{ annotation }
   {}
 
   [[nodiscard]] explicit constexpr Card(std::string id = "",
     std::string quantity = "",
     std::string name = "",
-    // std::string annotation,
     std::string set = "",
     std::string rarity = "",
     bool foil = false,
     double price = 0)
     : id_{ id }, name_{ name }, set_{ set }, quantity_{ quantity }, rarity_{ rarity }, foil_{ foil }, price_{ price }
-  //, annotation_{ annotation }
   {}
 
   std::string id_;
   std::string quantity_;
   std::string name_;
-  // std::string annotation_;// This attribute seems useless (yet to see it be not 0)
   std::string set_;
   std::string rarity_;
   bool foil_;
