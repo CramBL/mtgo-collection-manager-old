a0b54c32a4f91572e4ebc5038fb5c2a545aed1d0
Marc <marcbkonig@gmail.com>
2023-08-21 15:14:19 +0200

    bump versions

--- mtgoparser/Dependencies.cmake
+++ mtgoparser/Dependencies.cmake
@@ -13,7 +13,7 @@ function(mtgoparser_setup_dependencies)
   endif()
 
   if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#9.1.0")
+    cpmaddpackage("gh:fmtlib/fmt#10.1.0")
   endif()
 
   if(NOT TARGET spdlog::spdlog)
@@ -21,7 +21,7 @@ function(mtgoparser_setup_dependencies)
       NAME
       spdlog
       VERSION
-      1.11.0
+      1.12.0
       GITHUB_REPOSITORY
       "gabime/spdlog"
       OPTIONS
@@ -29,7 +29,7 @@ function(mtgoparser_setup_dependencies)
   endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+    cpmaddpackage("gh:catchorg/Catch2@3.4.0")
   endif()
 
   if(NOT TARGET tools::tools)
