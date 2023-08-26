b8ae5e27e90517e8e751fba017cdf225e51641a0
CramBL <marcbkonig@gmail.com>
2023-08-26 21:21:16 +0200

    add tmp mtgo_preprocessor main

---
+++ mtgoparser/src/mtgo_preprocessor/main.cpp
@@ -0,0 +1,51 @@
+#include "mtgoparser/goatbots.hpp"
+#include "mtgoparser/io.hpp"
+#include "mtgoparser/mtgo.hpp"
+#include <spdlog/spdlog.h>
+
+const auto path_trade_list_small_5cards = "./test/test-data/mtgo/Full Trade List-small-5cards.dek";
+const auto path_goatbots_card_defs_small = "./test/test-data/goatbots/card-defs-small-5cards.json";
+const auto path_goatbots_price_hist_small = "./test/test-data/goatbots/price-hist-small-5cards.json";
+
+auto example_collection_parse() -> int
+{
+  spdlog::info("=== example_collection_parse ===");
+  using goatbots::card_defs_map_t;
+  using goatbots::CardDefinition;
+  using goatbots::price_hist_map_t;
+
+  spdlog::info("==> parsing goatbots json...");
+  std::optional<card_defs_map_t> card_defs = goatbots::ReadJsonMap<card_defs_map_t>(path_goatbots_card_defs_small);
+  if (!card_defs.has_value()) {
+    // Error: ReadJsonMap() failed
+    return 1;
+  }
+  price_hist_map_t prices = goatbots::ReadJsonMap<price_hist_map_t>(path_goatbots_price_hist_small).value();
+  spdlog::info("==> parsing mtgo xml...");
+  auto cards = mtgo::xml::parse_dek_xml(path_trade_list_small_5cards);
+  auto collection = mtgo::Collection(std::move(cards));
+  spdlog::info("==> collection extract goatbots info...");
+  collection.ExtractGoatbotsInfo(card_defs.value(), prices);
+
+  spdlog::info("Collection size: {}", collection.Size());
+  spdlog::info("==> full collection print...");
+  collection.Print();
+  spdlog::info("==> collection to json...");
+  spdlog::info("{}", collection.ToJson());
+  spdlog::info("==> collection to pretty json...");
+  spdlog::info("{}", collection.ToJsonPretty());
+
+  spdlog::info("==> collection to json string...");
+  auto collection_json = collection.ToJson();
+  spdlog::info("==> override collection from json string...");
+  collection.FromJson(collection_json);
+  collection.Print();
+  spdlog::info("==> new collection from json string...");
+  auto new_collection = mtgo::Collection(collection_json);
+  spdlog::info("==> new collection print...");
+  new_collection.Print();
+
+  return 0;
+}
+
+int main() { return example_collection_parse(); }
