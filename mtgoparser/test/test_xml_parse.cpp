55ebbaa82bd4d7e66510b8dd26aa04a206c53134
CramBL <marcbkonig@gmail.com>
2023-08-26 11:43:00 +0200

    add sanity check for all trade list deck test files

--- mtgoparser/test/test_xml_parse.cpp
+++ mtgoparser/test/test_xml_parse.cpp
@@ -1,6 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 #include <mtgoparser/mtgo.hpp>
+#include <utility>
 
 const auto trade_list_file_path_small_5cards = "../../test/test-data/mtgo/Full Trade List-small-5cards.dek";
 const auto trade_list_file_path_small_50cards = "../../test/test-data/mtgo/Full Trade List-small-50cards.dek";
@@ -33,6 +35,17 @@ TEST_CASE("Card structs can be deserialized from XML", "[cards_from_xml]")
     CHECK(cards.at(4).name_ == "Tranquil Cove");
     CHECK(cards.at(4).id_ == "110465");
     CHECK(cards.at(4).quantity_ == "1");
+  }
+
+  SECTION("Sanity checks on all full trade list files")
+  {
+    const std::pair<const char *, std::size_t> test_file_card_count_pair =
+      GENERATE(std::make_pair(trade_list_file_path_small_5cards, 5),
+        std::make_pair(trade_list_file_path_small_50cards, 50),
+        std::make_pair(trade_list_file_path_small_500cards, 500),
+        std::make_pair(trade_list_file_path_medium_3000cards, 3000));
+
+    CHECK(mtgo::xml::parse_dek_xml(get<0>(test_file_card_count_pair)).size() == get<1>(test_file_card_count_pair));
   }
 }
 
