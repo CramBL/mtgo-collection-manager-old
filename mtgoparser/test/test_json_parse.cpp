1cebc6b3b3272e33e9c12c40bb9fb7f88856408a
CramBL <marcbkonig@gmail.com>
2023-08-26 14:27:53 +0200

    test card definitions

--- mtgoparser/test/test_json_parse.cpp
+++ mtgoparser/test/test_json_parse.cpp
@@ -6,15 +6,20 @@
 const auto path_goatbots_card_defs_small_5cards = "../../test/test-data/goatbots/card-defs-small-5cards.json";
 const auto path_goatbots_price_hist_small_5cards = "../../test/test-data/goatbots/price-hist-small-5cards.json";
 
-TEST_CASE("CardDefinition structs are correctly deserialized from Goatbots JSON", "[cards_from_goatbots_json]")
+TEST_CASE("CardDefinition structs are correctly deserialized from Goatbots JSON", "[card_defs_from_goatbots_json]")
 {
-
   using goatbots::card_defs_map_t;
   using goatbots::CardDefinition;
-  using goatbots::price_hist_map_t;
 
-  std::optional<card_defs_map_t> card_defs =
-    goatbots::ReadJsonMap<card_defs_map_t>(path_goatbots_card_defs_small_5cards);
+  card_defs_map_t card_defs = goatbots::ReadJsonMap<card_defs_map_t>(path_goatbots_card_defs_small_5cards).value();
 
-  SECTION("Sanity tests - Card definitions") { REQUIRE(card_defs.has_value()); }
+  SECTION("Sanity tests - Card definitions")
+  {
+    REQUIRE(card_defs.size() == 5);
+    CHECK(card_defs.contains("47483"));
+    CHECK(card_defs.contains("40516"));
+    CHECK(card_defs.contains("31745"));
+    CHECK(card_defs.contains("348"));
+    CHECK(card_defs.contains("347"));
+  }
 }
