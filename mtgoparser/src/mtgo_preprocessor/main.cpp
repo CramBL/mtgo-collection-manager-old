d6009d2365dc2bfddefd0e93e65eeb735e236f17
CramBL <marcbkonig@gmail.com>
2023-09-02 18:48:03 +0200

    add some simple command-line parsing

--- mtgoparser/src/mtgo_preprocessor/main.cpp
+++ mtgoparser/src/mtgo_preprocessor/main.cpp
@@ -1,15 +1,20 @@
 #include "mtgoparser/goatbots.hpp"
 #include "mtgoparser/io.hpp"
 #include "mtgoparser/mtgo.hpp"
+#include <algorithm>
 #include <internal_use_only/config.hpp>
+#include <optional>
 #include <spdlog/spdlog.h>
+#include <string_view>
+#include <type_traits>
+
+// TODO: VERY TEMPORARY NOLINT - AS SOON AS MAIN DOES SOMETHING USEFUL REMOVE THIS!!
+// NOLINTBEGIN
 
 const auto path_trade_list_small_5cards = "./test/test-data/mtgo/Full Trade List-small-5cards.dek";
 const auto path_goatbots_card_defs_small = "./test/test-data/goatbots/card-defs-small-5cards.json";
 const auto path_goatbots_price_hist_small = "./test/test-data/goatbots/price-hist-small-5cards.json";
 
-// TODO: VERY TEMPORARY NOLINT - AS SOON AS MAIN DOES SOMETHING USEFUL REMOVE THIS!!
-// NOLINTBEGIN
 
 auto example_collection_parse() -> int
 {
@@ -52,9 +57,87 @@ auto example_collection_parse() -> int
   return 0;
 }
 
-int main()
+// Command-Line Argument Parsing (CLAP) utility
+namespace clap {
+// Helper function to check if a value equals any element in a parameter pack
+template<typename T, typename... Args> constexpr bool equals_any(const T &value, Args... args)
 {
-  fmt::print("v{}\n", mtgoparser::cmake::project_version);
+  return ((value == args) || ...);
+}
+
+// Type trait to check if a type is convertible to std::string_view
+template<typename T> struct is_convertible_to_string_view
+{
+  static constexpr bool value = std::is_convertible_v<T, std::string_view>;
+};
+
+// Helper function to check if all types in a parameter pack are convertible to std::string_view
+template<typename... Args> constexpr bool all_convertible_to_string_view()
+{
+  return (is_convertible_to_string_view<Args>::value && ...);
+}
+
+// Check if an option or any of its aliases are set
+template<typename... Options>
+[[nodiscard]] auto has_option(const std::vector<std::string_view> &args, Options... option_names) -> bool
+{
+  static_assert(all_convertible_to_string_view<Options...>(), "Options must be convertible to std::string_view");
+
+  for (const auto &arg : args) {
+    if (equals_any(arg, option_names...)) { return true; }
+  }
+  return false;
+}
+
+// Returns the argument to an option if the option or any of its aliases exists and it has an argument
+template<typename... Options>
+[[nodiscard]] auto has_option_arg(const std::vector<std::string_view> &args, Options... option_names)
+  -> std::optional<std::string_view>
+{
+  static_assert(all_convertible_to_string_view<Options...>(), "Options must be convertible to std::string_view");
+
+
+  for (auto it = args.begin(), end = args.end(); it != end; ++it) {
+    if (equals_any(*it, option_names...)) {
+      if (it + 1 != end) {
+        return *(it + 1);
+      } else {
+        spdlog::error("Option {} was specified but no argument was given", *it);
+      }
+    }
+  }
+
+  return std::nullopt;
+}
+
+// Basic DIY command-line argument parsing
+[[nodiscard]] std::string_view get_option(const std::vector<std::string_view> &args,
+  const std::string_view &option_name)
+{
+  for (auto it = args.begin(), end = args.end(); it != end; ++it) {
+    if (*it == option_name)
+      if (it + 1 != end) return *(it + 1);
+  }
+
+  return "";
+}
+}// namespace clap
+
+int main(int argc, char *argv[])
+{
+  // Get command-line arguments as a vector of string_views
+  const std::vector<std::string_view> args(argv + 1, argv + argc);
+
+  if (auto option_arg = clap::has_option_arg(args, "--caller", "--calling")) {
+    fmt::print("Called from: {}\n", option_arg.value());
+  };
+
+  if (clap::has_option(args, "--echo")) {
+    for (const auto &arg : args) { fmt::print("{}\n", arg); }
+  }
+
+  if (clap::has_option(args, "--version", "-V")) { fmt::print("v{}\n", mtgoparser::cmake::project_version); }
+
   return example_collection_parse();
 }
 
