a7d4aebfffab980ca7ed7c46c27c046c3caee397
CramBL <marcbkonig@gmail.com>
2023-09-02 14:11:02 +0200

    add local project binary paths and tests

--- mtgoupdater/src/lib.rs
+++ mtgoupdater/src/lib.rs
@@ -1,11 +1,28 @@
 use std::process::Command;
 
+const MTGOGETTER_BIN: &str = if cfg!(windows) {
+    "../mtgogetter/mtgogetter.exe"
+} else {
+    "../mtgogetter/mtgogetter"
+};
+const MTGOPARSER_BIN: &str = if cfg!(windows) {
+    "../mtgoparser/build/src/mtgo_preprocessor/mtgo_preprocesser.exe"
+} else {
+    "../mtgoparser/build/src/mtgo_preprocessor/mtgo_preprocesser"
+};
+
 pub fn download_goatbots_price_history() -> Result<std::process::Output, Box<dyn std::error::Error>>
 {
-    let go_exec_out = Command::new("mtgogetter")
+    let go_exec_out = Command::new(MTGOGETTER_BIN)
         .arg("download")
         .arg("gph")
         .output()?;
+
+    Ok(go_exec_out)
+}
+
+pub fn run_mtgo_preprocessor() -> Result<std::process::Output, Box<dyn std::error::Error>> {
+    let go_exec_out = Command::new(MTGOPARSER_BIN).output()?;
 
     Ok(go_exec_out)
 }
@@ -18,13 +35,55 @@ mod tests {
     fn test_call_mtgogetter_download_price_history() {
         let test_out = download_goatbots_price_history();
         match test_out {
-            Ok(output) => println!(
-                "Status: {status}, stdout: {stdout:?}, stderr {stderr:?}",
-                status = output.status,
-                stdout = output.stdout,
-                stderr = output.stderr
-            ),
-            Err(e) => assert!(false, "Unexpected error: {e}"),
+            Ok(output) => {
+                println!("Status:\n{status}", status = output.status,);
+
+                println!(
+                    "stdout:\n{stdout}",
+                    stdout = String::from_utf8_lossy(&output.stdout),
+                );
+                println!(
+                    "stderr:\n{stderr}",
+                    stderr = String::from_utf8_lossy(&output.stderr),
+                );
+            }
+            Err(e) => panic!("Unexpected error: {e}"),
+        }
+    }
+
+    #[test]
+    fn test_call_mtgo_preprocessor() {
+        // Check the build directory exists
+        assert!(
+            std::path::Path::new("../mtgoparser/build").exists(),
+            "Build directory does not exist, build mtgoparser before running this test"
+        );
+        // Check the build src mtgo_preprocessor directory exists
+        assert!(
+            std::path::Path::new("../mtgoparser/build/src/mtgo_preprocessor").exists(),
+            "mtgo_preprocessor directory does not exist, build mtgoparser before running this test"
+        );
+        // Check the mtgo_preprocessor binary exists
+        assert!(
+            std::path::Path::new(MTGOPARSER_BIN).exists(),
+            "mtgo_preprocessor binary does not exist, build mtgoparser before running this test"
+        );
+
+        let test_out = run_mtgo_preprocessor();
+        match test_out {
+            Ok(output) => {
+                println!("Status:\n{status}", status = output.status,);
+
+                println!(
+                    "stdout:\n{stdout}",
+                    stdout = String::from_utf8_lossy(&output.stdout),
+                );
+                println!(
+                    "stderr:\n{stderr}",
+                    stderr = String::from_utf8_lossy(&output.stderr),
+                );
+            }
+            Err(e) => println!("Unexpected error: {e}"),
         }
     }
 }
