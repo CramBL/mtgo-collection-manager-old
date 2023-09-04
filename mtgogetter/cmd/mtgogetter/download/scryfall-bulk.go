207c8b8c82e73067fdd0c3cefa840e3981e86a7f
CramBL <marcbkonig@gmail.com>
2023-09-04 18:06:39 +0200

    complete get scryfall bulk data json subcmd

--- mtgogetter/cmd/mtgogetter/download/scryfall-bulk.go
+++ mtgogetter/cmd/mtgogetter/download/scryfall-bulk.go
@@ -5,13 +5,14 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 type Message struct {
-	download_uri string
+	Download_uri string `json:"download_uri"`
 }
 
 const ScryfallInfoBulkUrl string = "https://api.scryfall.com/bulk-data/e2ef41e3-5778-4bc2-af3f-78eca4dd9c23"
@@ -33,6 +34,7 @@ The data comes as a JSON file containing every card object on Scryfall in Englis
 			log.Fatal(err)
 		}
 		defer resp.Body.Close()
+
 		if resp.StatusCode != 200 {
 			log.Fatalln("Get returned:", resp.StatusCode, http.StatusText(resp.StatusCode))
 		}
@@ -43,13 +45,38 @@ The data comes as a JSON file containing every card object on Scryfall in Englis
 		}
 
 		var msg Message
-		err = json.Unmarshal(bodyAsBytes, &msg)
-		if err != nil {
+		if err:= json.Unmarshal(bodyAsBytes, &msg); err != nil {
 			log.Fatalln("Error when Unmarshalling JSON:", err)
 		}
+		log.Println("Got download uri:", msg.Download_uri)
 
-		log.Println("Got download uri:", string(msg))
-		var split_msg = strings.SplitAfter(msg.download_uri, "default-cards-")
-		log.Println("Split message:", split_msg)
+		var split_msg = strings.SplitAfter(msg.Download_uri, "default-cards-")
+		// Concatenate the end (timestamp + .json) of the received download_uri with what we know the prefix URL should be
+		// simply to not blindly call GET on unsanitized received URL
+		download_url := templateDownloadUri + split_msg[1]
+		log.Println("Download URL to get bulk data from:", download_url)
+
+		resp_bulk_data, err := http.Get(download_url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if resp_bulk_data.StatusCode != 200 {
+			log.Fatalln("Get returned:", resp_bulk_data.StatusCode, http.StatusText(resp_bulk_data.StatusCode))
+		}
+		defer resp.Body.Close()
+
+		// Name is default-cards-<timestamp>.json
+		fname := "default-cards-" + split_msg[1]
+
+		// Write resp_bulk_data.Body to file
+		bulk_data, err :=io.ReadAll(resp_bulk_data.Body)
+		if err != nil {
+			log.Println("Error reading bulk data response body:", err)
+		}
+
+		// Write JSON to disk
+		if err:= os.WriteFile(fname, bulk_data , 0777); err != nil {
+			log.Println("Error creating file:", err)
+		}
 	},
 }
