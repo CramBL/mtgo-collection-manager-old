3e87fae1e6bb8624e18a3ccbe86680ca72ca4678
CramBL <marcbkonig@gmail.com>
2023-09-04 20:18:21 +0200

    add more fields from scryfall json

--- mtgogetter/pkg/mtgogetter/scryfall_parse.go
+++ mtgogetter/pkg/mtgogetter/scryfall_parse.go
@@ -10,10 +10,13 @@ type ScryfallCard struct{
     Mtgo_id int32 `json:"mtgo_id"`
 	Mtgo_foil_id int32 `json:"mtgo_foil_id"`
 	Name string `json:"name"`
-	Prices struct {
+	Released_at string `json:"released_at"`
+	Rarity string `json:"rarity"`
+	Prices struct { // All nullable (deserialized as empty string if null)
 		Usd string `json:"usd"`
 		Usd_foil string `json:"usd_foil"`
 		Eur string `json:"eur"`
+		Eur_foil string `json:"eur_foil"`
 		Tix string `json:"tix"`
 	} `json:"prices"`
 }
