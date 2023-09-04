9d02bca1d1450b22fb01fc9abebb368f4f4847f8
CramBL <marcbkonig@gmail.com>
2023-09-04 21:25:59 +0200

    better logging

--- mtgogetter/cmd/mtgogetter/download/scryfall-bulk.go
+++ mtgogetter/cmd/mtgogetter/download/scryfall-bulk.go
@@ -29,7 +29,7 @@ var DownloadScryfallBulkCmd = &cobra.Command{
 The data comes as a JSON file containing every card object on Scryfall in English or the printed language if the card is only available in one language.`,
 	Args: cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("Downloading from", ScryfallInfoBulkUrl)
+		log.Println("GET bulk data info from:", ScryfallInfoBulkUrl)
 		resp, err := http.Get(ScryfallInfoBulkUrl)
 		if err != nil {
 			log.Fatal(err)
@@ -37,7 +37,7 @@ The data comes as a JSON file containing every card object on Scryfall in Englis
 		defer resp.Body.Close()
 
 		if resp.StatusCode != 200 {
-			log.Fatalln("Get returned:", resp.StatusCode, http.StatusText(resp.StatusCode))
+			log.Fatalln("GET returned:", resp.StatusCode, http.StatusText(resp.StatusCode))
 		}
 
 		bodyAsBytes, err := io.ReadAll(resp.Body)
@@ -49,13 +49,13 @@ The data comes as a JSON file containing every card object on Scryfall in Englis
 		if err:= json.Unmarshal(bodyAsBytes, &msg); err != nil {
 			log.Fatalln("Error when Unmarshalling JSON:", err)
 		}
-		log.Println("Got download uri:", msg.Download_uri)
+		log.Println("Response contained download uri:", msg.Download_uri)
 
 		var split_msg = strings.SplitAfter(msg.Download_uri, "default-cards-")
 		// Concatenate the end (timestamp + .json) of the received download_uri with what we know the prefix URL should be
 		// simply to not blindly call GET on unsanitized received URL
 		download_url := templateDownloadUri + split_msg[1]
-		log.Println("Download URL to get bulk data from:", download_url)
+		log.Println("Downloading bulk data from:", download_url)
 
 		resp_bulk_data, err := http.Get(download_url)
 		if err != nil {
@@ -69,17 +69,21 @@ The data comes as a JSON file containing every card object on Scryfall in Englis
 		// Name is default-cards-<timestamp>.json
 		fname := "default-cards-" + split_msg[1]
 
-		// Write resp_bulk_data.Body to file
+		log.Println("Streaming bulk data response body to byte array")
+		// Read response body to bytes
 		bulk_data, err :=io.ReadAll(resp_bulk_data.Body)
 		if err != nil {
 			log.Println("Error reading bulk data response body:", err)
 		}
 
+		log.Println("Deserializing raw scryfall JSON")
 		// Deserialize to the ScryfallCard struct (Taking only the fields we need)
 		scryfall_cards, err := mtgogetter.DeserializeScryfallCards(bulk_data)
 		if err != nil {
 			log.Fatalln("Error when deserializing Scryfall JSON:", err)
 		}
+
+		log.Println("Serializing scryfall card array to JSON and writing to disk as:", fname)
 		// Serialize to JSON string and write to disk
 		if err := mtgogetter.ScryfallCardsToDisk(scryfall_cards, fname); err != nil {
 			log.Fatalln("Error when writing Scryfall JSON to disk:", err)
