54e2f7d9a9a7a7102b8d98c9960a29bbd7044348
CramBL <marcbkonig@gmail.com>
2023-09-05 18:00:01 +0200

    add explicit default ctor to quell false positive warnings

--- mtgoparser/include/mtgoparser/mtgo/card.hpp
+++ mtgoparser/include/mtgoparser/mtgo/card.hpp
@@ -8,8 +8,19 @@
 namespace mtgo {
 struct Card
 {
-  [[nodiscard]] explicit Card() = default;
+  // Default constructor
+  // Note: some builds raises false positives in static analysis when simply declared as `Card() = default` )
+  [[nodiscard]] explicit Card(std::string id = {},
+    std::string quantity = {},
+    std::string name = {},
+    std::string set = {},
+    std::string rarity = {},
+    bool foil = {},
+    double price = {}) noexcept
+    : id_{ id }, quantity_{ quantity }, name_{ name }, set_{ set }, rarity_{ rarity }, foil_{ foil }, price_{ price }
+  {}
 
+  // Partially parametrised constructor used to construct a card from MTGO .dek XML
   [[nodiscard]] explicit Card(const char *id,
     const char *quantity,
     const char *name,
@@ -20,18 +31,20 @@ struct Card
     : id_{ id }, quantity_{ quantity }, name_{ name }, set_{ set }, rarity_{ rarity }, foil_{ foil }, price_{ price }
   {}
 
-
+  // Templated constructor
   template<typename T>
-  requires std::convertible_to<T, std::string>
-  explicit Card(T id, T quantity, T name, T set, T rarity = "", bool foil = false, double price = 0) noexcept
+    requires std::convertible_to<T, std::string>
+  explicit Card(T id, T quantity, T name, T set, T rarity, bool foil = false, double price = 0) noexcept
     : id_{ id }, quantity_{ quantity }, name_{ name }, set_{ set }, rarity_{ rarity }, foil_{ foil }, price_{ price }
   {}
 
+  // Move constructor
   [[nodiscard]] Card(Card &&other) noexcept
     : id_(std::move(other.id_)), quantity_(std::move(other.quantity_)), name_(std::move(other.name_)),
       set_(std::move(other.set_)), rarity_(std::move(other.rarity_)), foil_(other.foil_), price_(other.price_)
   {}
 
+  // Move assignment operator
   [[nodiscard]] Card &operator=(Card &&other) noexcept
   {
     if (this != &other) {
