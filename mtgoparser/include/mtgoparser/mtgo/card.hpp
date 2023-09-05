1565f8d842c8d2b9f0e094cf6be0b9132d050e9e
CramBL <marcbkonig@gmail.com>
2023-09-05 11:16:34 +0200

    implement move semantics

--- mtgoparser/include/mtgoparser/mtgo/card.hpp
+++ mtgoparser/include/mtgoparser/mtgo/card.hpp
@@ -37,6 +37,26 @@ struct Card
     : id_{ id }, quantity_{ quantity }, name_{ name }, set_{ set }, rarity_{ rarity }, foil_{ foil }, price_{ price }
   {}
 
+  [[nodiscard]] Card(Card &&other) noexcept
+    : id_(std::move(other.id_)), quantity_(std::move(other.quantity_)), name_(std::move(other.name_)),
+      set_(std::move(other.set_)), rarity_(std::move(other.rarity_)), foil_(other.foil_), price_(other.price_)
+  {}
+
+  [[nodiscard]] Card &operator=(Card &&other) noexcept
+  {
+    if (this != &other) {
+      id_ = std::move(other.id_);
+      quantity_ = std::move(other.quantity_);
+      name_ = std::move(other.name_);
+      set_ = std::move(other.set_);
+      rarity_ = std::move(other.rarity_);
+      foil_ = other.foil_;
+      price_ = other.price_;
+    }
+
+    return *this;
+  }
+
   std::string id_;
   std::string quantity_;
   std::string name_;
