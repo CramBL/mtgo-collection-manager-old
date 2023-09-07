1f382a4e988ed423f1c0645a003929dc2e066dc1
CramBL <marcbkonig@gmail.com>
2023-09-07 09:10:04 +0200

    more utility functions

--- mtgogetter/pkg/mtgogetter/mtgogetter.go
+++ mtgogetter/pkg/mtgogetter/mtgogetter.go
@@ -83,15 +83,41 @@ func FirstFileFromZipToDisk(fname string, zip_reader *zip.Reader) {
 	}
 
 	// Create file on disk for writing
-	fd, err := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, first_file_reader.Mode())
+	ReadCloserToDisk(first_file, fname)
 	if err != nil {
-		log.Fatalln("Error creating file:", err)
+		log.Fatalln("Error writing file:", err)
+	}
+}
+
+func FirstFileFromZip(zip_reader *zip.Reader) (io.ReadCloser, error) {
+	first_file_reader := zip_reader.File[0]
+
+	first_file, err := first_file_reader.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	return first_file, nil
+}
+
+func ReadCloserToStdout(read_closer io.ReadCloser) (int64, error) {
+	written_bytes, err := io.Copy(os.Stdout, read_closer)
+	if err != nil {
+		return written_bytes, err
+	}
+	return written_bytes, nil
+}
+
+func ReadCloserToDisk(read_closer io.ReadCloser, fname string) (int64, error) {
+	fd, err := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+	if err != nil {
+		return 0, err
 	}
 	defer fd.Close()
 
-	// Copy the file from the archive to the created file on disk
-	_, err = io.Copy(fd, first_file)
+	written_bytes, err := io.Copy(fd, read_closer)
 	if err != nil {
-		log.Fatalln("Error extracting file:", err)
+		return written_bytes, err
 	}
+	return written_bytes, nil
 }
