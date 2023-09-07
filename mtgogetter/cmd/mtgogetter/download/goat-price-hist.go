22f6ccd7baa307f2eda1d0eaa3d4e4f3bd26bf09
CramBL <marcbkonig@gmail.com>
2023-09-07 09:38:16 +0200

    allow outputting to stdout

--- mtgogetter/cmd/mtgogetter/download/goat-price-hist.go
+++ mtgogetter/cmd/mtgogetter/download/goat-price-hist.go
@@ -1,6 +1,8 @@
 package download
 
 import (
+	"log"
+
 	"github.com/CramBL/mtgo-collection-manager/mtgogetter/pkg/mtgogetter"
 	"github.com/spf13/cobra"
 )
@@ -18,6 +20,23 @@ The price history appears as a JSON map of unique card IDs and associated tix pr
 	Run: func(cmd *cobra.Command, args []string) {
 		dl_bytes := mtgogetter.DownloadBodyToBytes(GoatbotsPriceHistoryUrl)
 		reader := mtgogetter.UnzipFromBytes(dl_bytes)
-		mtgogetter.FirstFileFromZipToDisk("price-history.json", reader)
+		first_file_from_zip, err := mtgogetter.FirstFileFromZip(reader)
+		if err != nil {
+			log.Fatalln("Error opening first file from zip archive: ", err)
+		}
+
+		// If the --save-as flag was not set (or is set to stdout), print to stdout
+		if mtgogetter.OutputIsStdout(cmd) {
+			_, err := mtgogetter.ReadCloserToStdout(first_file_from_zip)
+			if err != nil {
+				log.Fatalln("Error printing to stdout:", err)
+			}
+		} else {
+			fname := cmd.Flag("save-as").Value.String()
+			_, err := mtgogetter.ReadCloserToDisk(first_file_from_zip, fname)
+			if err != nil {
+				log.Fatalln("Error writing file:", err)
+			}
+		}
 	},
 }
