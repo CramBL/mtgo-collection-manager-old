44cf22f9d4f4794ecdccbbd75bdf15402d29570c
CramBL <marcbkonig@gmail.com>
2023-09-07 22:57:00 +0200

    add option to decompress or save as file

--- mtgoupdater/src/lib.rs
+++ mtgoupdater/src/lib.rs
@@ -31,13 +31,20 @@ pub fn download_goatbots_card_definitions(
     Ok(go_exec_out)
 }
 
-pub fn download_custom_url(url: &str) -> Result<std::process::Output, Box<dyn std::error::Error>> {
-    let go_exec_out = Command::new(MTGOGETTER_BIN)
-        .arg("download")
-        .arg("custom")
-        .arg("url-raw")
-        .arg(url)
-        .output()?;
+pub fn download_custom_url(
+    url: &str,
+    decompress: bool,
+    save_as: Option<&str>,
+) -> Result<std::process::Output, Box<dyn std::error::Error>> {
+    let mut custom_args = vec!["download", "custom", "url-raw", url];
+    if decompress {
+        custom_args.push("--decompress");
+    }
+    if let Some(save_as) = save_as {
+        custom_args.push("--save-as");
+        custom_args.push(save_as);
+    }
+    let go_exec_out = Command::new(MTGOGETTER_BIN).args(custom_args).output()?;
 
     Ok(go_exec_out)
 }
