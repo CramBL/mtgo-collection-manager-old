0104e97c04b8a4ab4e23cb7b172ec38802a676ed
CramBL <marcbkonig@gmail.com>
2023-09-07 09:10:36 +0200

    move save as flag to download base command

--- mtgogetter/cmd/mtgogetter/download/custom.go
+++ mtgogetter/cmd/mtgogetter/download/custom.go
@@ -9,14 +9,10 @@ var BaseDownloadCustomCmd = &cobra.Command{
 	Use:     "custom",
 	Aliases: []string{"c", "url", "endpoint"},
 	Short:   "Download content from a specified URL and save it to a file",
-	Args: cobra.ExactArgs(0),
+	Args:    cobra.ExactArgs(0),
 }
 
-
-
 func init() {
-	var save_as_file_name string
-	BaseDownloadCustomCmd.PersistentFlags().StringVarP(&save_as_file_name, "save-as", "s", "mtgogetter_tmp.txt", "Write downloaded content to specified filename")
 	var do_decompress bool
 	BaseDownloadCustomCmd.PersistentFlags().BoolVarP(&do_decompress, "decompress", "d", false, "Specify that the downloaded content should be decompressed before writing to disk")
 	BaseDownloadCustomCmd.AddCommand(custom.DownloadCustomUrlStringCmd)
