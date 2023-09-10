ee37343da1f68a87b8a0545f20d7d88e600bcd2d
CramBL <marcbkonig@gmail.com>
2023-09-10 22:59:06 +0200

    add update and unlock method

--- mtgogetter/pkg/mtgogetter/state_log_accessor.go
+++ mtgogetter/pkg/mtgogetter/state_log_accessor.go
@@ -35,7 +35,7 @@ func GetStateLogAccessor() (*StateLogAccesor, error) {
 }
 
 // Get the state log.
-// REMEMBER TO CALL ReleaseStateLog() WHEN DONE (use defer)
+// REMEMBER TO CALL ReleaseStateLog() or another unlocking method when done (use defer if possible)
 func (s *StateLogAccesor) GetStateLog() *StateLog {
 	s.mut.Lock()
 	return s.state_log
@@ -52,6 +52,20 @@ func (s *StateLogAccesor) UpdateStateLog(update_action func(*StateLog)) error {
 	defer s.mut.Unlock()
 
 	update_action(s.state_log)
+
+	// Write the state log to disk
+	err := writeStateLogToFile(s.state_log)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Update and unlock an already locked state log
+// Runtime error if the state log is not locked
+func (s *StateLogAccesor) UpdateAndUnlockStateLog(update_action func(*StateLog)) error {
+	update_action(s.state_log)
+	defer s.mut.Unlock()
 
 	// Write the state log to disk
 	err := writeStateLogToFile(s.state_log)
