e4b438488239dccb4d93900d1a6402b1da8b15d4
CramBL <marcbkonig@gmail.com>
2023-09-10 14:25:43 +0200

    add tests for time stamps

--- mtgogetter/pkg/mtgogetter/state_log_test.go
+++ mtgogetter/pkg/mtgogetter/state_log_test.go
@@ -59,3 +59,67 @@ func TestStateLogDeserialize(t *testing.T) {
 		t.Errorf("Expected the timestamps to be equal to the unix epoch, got %d", state_log.Scryfall.Updated_at.Unix())
 	}
 }
+
+func TestStateLogGoatbotsUpdateTime_PriceIsOutdated(t *testing.T) {
+	// Test that the PriceUpdateAvailable function works as expected
+	state_log := NewStateLog()
+
+	// Set the updated_at timestamps to 3:31 AM UTC (then there should be a new updated prices available)
+	utc_now := time.Now().UTC()
+	utc_4am := time.Date(utc_now.Year(), utc_now.Month(), utc_now.Day(), 4, 0, 0, 0, time.UTC)
+	if utc_now.Before(utc_4am) {
+		utc_3_31am_yesterday := time.Date(utc_now.Year(), utc_now.Month(), utc_now.Day()-1, 3, 31, 0, 0, time.UTC)
+		state_log.Goatbots.Prices_updated_at = time.Unix(utc_3_31am_yesterday.Unix(), 0)
+	} else {
+		utc_3_31am := time.Date(utc_now.Year(), utc_now.Month(), utc_now.Day(), 3, 31, 0, 0, time.UTC)
+		state_log.Goatbots.Prices_updated_at = time.Unix(utc_3_31am.Unix(), 0)
+	}
+
+	fmt.Println(state_log.Goatbots.Prices_updated_at.String())
+
+	// Test that the returns false when the updated_at timestamp is before 4 AM yesterday
+	if state_log.Goatbots.IsPriceUpdated() {
+		t.Errorf("Expected PriceUpdateAvailable to return true")
+	}
+}
+
+func TestStateLogGoatbotsUpdateTime_PriceIsUpdated(t *testing.T) {
+	// Test that the PriceUpdateAvailable function works as expected
+	state_log := NewStateLog()
+
+	// Set the updated_at timestamps to 4:01 am UTC (then it should be up-to-date)
+	utc_now := time.Now().UTC()
+	utc_4am := time.Date(utc_now.Year(), utc_now.Month(), utc_now.Day(), 4, 0, 0, 0, time.UTC)
+	if utc_now.Before(utc_4am) {
+		utc_4_01am_yesterday := time.Date(utc_now.Year(), utc_now.Month(), utc_now.Day()-1, 4, 1, 0, 0, time.UTC)
+		state_log.Goatbots.Prices_updated_at = time.Unix(utc_4_01am_yesterday.Unix(), 0)
+	} else {
+		utc_4_01am := time.Date(utc_now.Year(), utc_now.Month(), utc_now.Day(), 4, 1, 0, 0, time.UTC)
+		state_log.Goatbots.Prices_updated_at = time.Unix(utc_4_01am.Unix(), 0)
+	}
+
+	fmt.Println(state_log.Goatbots.Prices_updated_at.UTC().String())
+
+	// Test that the function returns true when the updated_at timestamp is after 4 AM yesterday
+	if !state_log.Goatbots.IsPriceUpdated() {
+		t.Errorf("Expected PriceUpdateAvailable to return true for: %s", state_log.Goatbots.Prices_updated_at.UTC().String())
+	}
+}
+
+func TestStateLogGoatbotsUpdateTime_PriceIsUpdated_local(t *testing.T) {
+	// Test that the PriceUpdateAvailable function works as expected
+	state_log := NewStateLog()
+
+	// Set the updated_at timestamp to the local time 25 hours ago (then it should be time to update)
+	local_now := time.Now().Local()
+	local_24_hours_ago := time.Date(local_now.Year(), local_now.Month(), local_now.Day()-1, local_now.Hour() , local_now.Minute(), 0, 0, local_now.Location())
+	local_25_hours_ago := local_24_hours_ago.Add(-1 * time.Hour)
+
+	state_log.Goatbots.Prices_updated_at = time.Unix(local_25_hours_ago.Unix(), 0)
+	fmt.Println(state_log.Goatbots.Prices_updated_at.String())
+
+	// Test that the function returns true when the updated_at timestamp is after 4 AM yesterday
+	if state_log.Goatbots.IsPriceUpdated() {
+		t.Errorf("Expected PriceUpdateAvailable to return true for: %s", state_log.Goatbots.Prices_updated_at.String())
+	}
+}
