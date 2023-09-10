cb256003bb4a3495ff4bb82df93709d4086bde39
CramBL <marcbkonig@gmail.com>
2023-09-10 12:57:22 +0200

    better and more testing

--- mtgogetter/pkg/mtgogetter/state_log_test.go
+++ mtgogetter/pkg/mtgogetter/state_log_test.go
@@ -1,35 +1,56 @@
 package mtgogetter_test
 
 import (
+	"bytes"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/BurntSushi/toml"
 	. "github.com/CramBL/mtgo-collection-manager/mtgogetter/pkg/mtgogetter"
 )
 
-var state_log_str string = `title = "log for mtgogetter state, such as updated_at timestamps"
-	[goatbots]
-	updated_at = "1970-01-01T00:00:00Z"
 
-	[scryfall]
-	updated_at = "1970-01-01T00:00:00Z"`
 
 func TestStateLogDeserialize(t *testing.T) {
+	// Make default statelog and convert it to TOML and then to a string
+	state_log_default := NewStateLog()
+
+	var state_log_buf bytes.Buffer
+	if err := toml.NewEncoder(&state_log_buf).Encode(state_log_default); err != nil {
+		t.Errorf("Error when encoding default StateLog to TOML: %s", err)
+	}
+
+	state_log_str := state_log_buf.String()
+
+	// Deserialize the TOML into a StateLog struct
 	var state_log StateLog
 
 	if _, err := toml.Decode(state_log_str, &state_log); err != nil {
 		t.Errorf("Error when parsing StateLog TOML: %s", err)
 	}
 
-	if state_log.Title != "log for mtgogetter state, such as updated_at timestamps" {
-		t.Errorf("Expected 'log for mtgogetter state, such as updated_at timestamps' got %s", state_log.Title)
+	if state_log.Title != "log for MTGO Getter state, such as updated_at timestamps" {
+		t.Errorf("Expected 'log for MTGO Getter state, such as updated_at timestamps' got %s", state_log.Title)
 	}
 
-	if state_log.Goatbots.Updated_at.String() != "1970-01-01 00:00:00 +0000 UTC" {
-		t.Errorf("Expected '1970-01-01T00:00:00Z' got %s", state_log.Goatbots.Updated_at.String())
+	if !strings.Contains(state_log.Goatbots.Updated_at.String(), "1970-01-01 01:00:00") {
+		t.Errorf("Expected updated_at to contain '1970-01-01 01:00:00' got %s", state_log.Goatbots.Updated_at.String())
 	}
 
 	if state_log.Scryfall.Updated_at.Day() != 1 {
 		t.Errorf("Expected 1 got %d", state_log.Scryfall.Updated_at.Day())
+	}
+	if state_log.Scryfall.Updated_at.Hour() != 1 {
+		t.Errorf("Expected 1 got %d", state_log.Scryfall.Updated_at.Hour())
+	}
+
+	if !state_log.Scryfall.Updated_at.Equal(state_log.Goatbots.Updated_at) {
+		t.Errorf("Expected Goatbots and Scryfall updated_at to be the same")
+	}
+
+	// Check that the updated_at timestamps are equal to Unix epoch
+	if !state_log.Scryfall.Updated_at.Equal(time.Unix(0, 0)) {
+		t.Errorf("Expected the timestamps to be equal to the unix epoch, got %d", state_log.Scryfall.Updated_at.Unix())
 	}
 }
