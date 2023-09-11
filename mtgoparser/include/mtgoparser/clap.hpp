4cdebaf46b927b5aba48e82767847b60cad846ed
CramBL <marcbkonig@gmail.com>
2023-09-11 15:33:32 +0200

    implement get option value (and move helper namespace)

--- mtgoparser/include/mtgoparser/clap.hpp
+++ mtgoparser/include/mtgoparser/clap.hpp
@@ -8,6 +8,59 @@
 
 // Command-Line Argument Parsing (CLAP) utility
 namespace clap {
+namespace {// Utility used by the Clap class
+
+  // Helper function to check if a value equals any element in a parameter pack
+  template<typename T, typename... Args> constexpr auto equals_any(const T &value, Args... args) -> bool
+  {
+    return ((value == args) || ...);
+  }
+
+  // Type trait to check if a type is convertible to std::string_view
+  template<typename T> struct is_convertible_to_string_view
+  {
+    static constexpr bool value = std::is_convertible_v<T, std::string_view>;
+  };
+
+  // Helper function to check if all types in a parameter pack are convertible to std::string_view
+  template<typename... Args> constexpr bool all_convertible_to_string_view()
+  {
+    return (is_convertible_to_string_view<Args>::value && ...);
+  }
+
+  // Check if an option or any of its aliases are set
+  template<typename... Options>
+  [[nodiscard]] auto has_option(const std::vector<std::string_view> &args, Options... option_names) -> bool
+  {
+    static_assert(all_convertible_to_string_view<Options...>(), "Options must be convertible to std::string_view");
+
+    // Cannot use std::ranges because apple clang still does not support it...
+    return std::any_of(
+      args.begin(), args.end(), [&](const std::string_view &arg) { return equals_any(arg, option_names...); });
+  }
+
+  // Returns the argument to an option if the option or any of its aliases exists and it has an argument
+  template<typename... Options>
+  [[nodiscard]] auto has_option_arg(const std::vector<std::string_view> &args, Options... option_names)
+    -> std::optional<std::string_view>
+  {
+    static_assert(all_convertible_to_string_view<Options...>(), "Options must be convertible to std::string_view");
+
+
+    for (auto it = args.begin(), end = args.end(); it != end; ++it) {
+      if (equals_any(*it, option_names...)) {
+        if (it + 1 != end) {
+          return *(it + 1);
+        } else {
+          spdlog::error("Option {} was specified but no argument was given", *it);
+        }
+      }
+    }
+
+    return std::nullopt;
+  }
+
+}// namespace
 
 template<size_t N_options> class Clap
 {
@@ -37,68 +90,26 @@ public:
       spdlog::warn("No arguments found - did you remember to parse them first?");
     }
   }
-};
 
-
-// Helper function to check if a value equals any element in a parameter pack
-template<typename T, typename... Args> constexpr bool equals_any(const T &value, Args... args)
-{
-  return ((value == args) || ...);
-}
-
-// Type trait to check if a type is convertible to std::string_view
-template<typename T> struct is_convertible_to_string_view
-{
-  static constexpr bool value = std::is_convertible_v<T, std::string_view>;
-};
-
-// Helper function to check if all types in a parameter pack are convertible to std::string_view
-template<typename... Args> constexpr bool all_convertible_to_string_view()
-{
-  return (is_convertible_to_string_view<Args>::value && ...);
-}
-
-// Check if an option or any of its aliases are set
-template<typename... Options>
-[[nodiscard]] auto has_option(const std::vector<std::string_view> &args, Options... option_names) -> bool
-{
-  static_assert(all_convertible_to_string_view<Options...>(), "Options must be convertible to std::string_view");
-
-  // Cannot use std::ranges because apple clang still does not support it...
-  return std::any_of(
-    args.begin(), args.end(), [&](const std::string_view &arg) { return equals_any(arg, option_names...); });
-}
-
-// Returns the argument to an option if the option or any of its aliases exists and it has an argument
-template<typename... Options>
-[[nodiscard]] auto has_option_arg(const std::vector<std::string_view> &args, Options... option_names)
-  -> std::optional<std::string_view>
-{
-  static_assert(all_convertible_to_string_view<Options...>(), "Options must be convertible to std::string_view");
-
-
-  for (auto it = args.begin(), end = args.end(); it != end; ++it) {
-    if (equals_any(*it, option_names...)) {
-      if (it + 1 != end) {
-        return *(it + 1);
-      } else {
-        spdlog::error("Option {} was specified but no argument was given", *it);
-      }
+  template<std::convertible_to<std::string_view>... Flags> [[nodiscard]] auto FlagSet(Flags... flags) -> bool
+  {
+    if (!_args.has_value()) {
+      spdlog::warn("Attempted to check if a CL flag was set before parsing CL arguments");
+      return false;
     }
+    return has_option(_args.value(), flags...);
   }
 
-  return std::nullopt;
-}
-
-// Basic DIY command-line argument parsing
-[[nodiscard]] std::string_view get_option(const std::vector<std::string_view> &args,
-  const std::string_view &option_name)
-{
-  for (auto it = args.begin(), end = args.end(); it != end; ++it) {
-    if (*it == option_name)
-      if (it + 1 != end) return *(it + 1);
+  template<std::convertible_to<std::string_view>... Options>
+  [[nodiscard]] auto OptionValue(Options... opts) -> std::optional<std::string_view>
+  {
+    if (!_args.has_value()) {
+      spdlog::warn("Attempted to retrieve an CL option value before parsing CL arguments");
+      return std::nullopt;
+    }
+    return has_option_arg(_args.value(), opts...);
   }
+};
 
-  return "";
-}
+
 }// namespace clap
