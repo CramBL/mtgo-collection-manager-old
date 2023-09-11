5396aa5937eca752d614fc212af2d3121c399ca4
CramBL <marcbkonig@gmail.com>
2023-09-11 15:33:52 +0200

    add clap get option value test

--- mtgoparser/test/tests.cpp
+++ mtgoparser/test/tests.cpp
@@ -3,11 +3,11 @@
 
 constinit auto static_clap = clap::Clap<1>("--version");
 
-TEST_CASE("Test misc. stuff")
+TEST_CASE("Test basic CLAP")
 {
 
-  char argv0[] = "mtgo_preprocessor\0";
-  char argv1[] = "--version\0";
+  char argv0[] = "mtgo_preprocessor";
+  char argv1[] = "--version";
 
   char *argv[] = { argv0, argv1 };
   int argc = 2;
@@ -31,9 +31,40 @@ TEST_CASE("Test misc. stuff")
     static_clap.PrintArgs();
   }
 
-  SECTION("Echo")
+  SECTION("Alias version cmd - Show version")
   {
-    auto clap_2opts = clap::Clap<2>("--version", "--echo");
-    clap_2opts.PrintOptions();
+    auto clap_alias_version = clap::Clap<2>("--version", "-V");
+    clap_alias_version.Parse(argc, argv);
+
+    fmt::print("Arguments are:\n");
+    clap_alias_version.PrintArgs();
+
+    CHECK(clap_alias_version.FlagSet("--version"));
+    CHECK(clap_alias_version.FlagSet("--version", "-V"));
+    CHECK(!clap_alias_version.FlagSet("-V"));
+  }
+}
+
+TEST_CASE("Test CLAP with options and values")
+{
+
+  char argv0[] = "mtgo_preprocessor";
+  char arg_version[] = "--version";
+  char arg_save_as[] = "--save-as";
+  char arg_save_as_val[] = "saved.txt";
+
+  SECTION("test save as option value")
+  {
+    char *argv[] = { argv0, arg_save_as, arg_save_as_val };
+    int argc = 3;
+
+    auto clap = clap::Clap<4>("--version", "-V", "--save-as", "-s");
+
+    clap.Parse(argc, argv);
+    fmt::print("Got args:\n");
+    clap.PrintArgs();
+
+    CHECK(clap.OptionValue("--save-as", "-s").value() == arg_save_as_val);
+    CHECK(clap.FlagSet("--version", "-V") == false);
   }
 }
