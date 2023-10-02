e5ba4f55cadc9d88e321c25e819632ca7e14398f
CramBL <marcbkonig@gmail.com>
2023-10-02 19:04:11 +0200

    add example json call

--- mtgoupdater/tests/mtgoupdater_mtgo_preprocessor.rs
+++ mtgoupdater/tests/mtgoupdater_mtgo_preprocessor.rs
@@ -90,3 +90,28 @@ fn test_call_mtgo_preprocessor_gui_example() {
         Err(e) => panic!("Unexpected error: {e}"),
     }
 }
+
+#[test]
+fn test_call_mtgo_preprocessor_example_collection_json_stdout() {
+    internal_only::dev_try_init_mtgoparser_bin();
+    match mtgoupdater::internal_only::run_mtgo_preprocessor_example_collection_json_stdout() {
+        Ok(output) => {
+            println!("Status:\n{status}", status = output.status,);
+
+            println!(
+                "stdout:\n{stdout}",
+                stdout = String::from_utf8_lossy(&output.stdout),
+            );
+            println!(
+                "stderr:\n{stderr}",
+                stderr = String::from_utf8_lossy(&output.stderr),
+            );
+            assert!(
+                output.status.success(),
+                "Process failed with non-zero exit code: {}",
+                output.status.code().unwrap_or(123)
+            );
+        }
+        Err(e) => panic!("Unexpected error: {e}"),
+    }
+}
