a86ae3cf179aada60d00b666518aeb352c016aae
CramBL <marcbkonig@gmail.com>
2023-10-04 14:41:59 +0200

    add util header

---
+++ mtgoparser/include/mtgoparser/util.hpp
@@ -0,0 +1,18 @@
+#include <charconv>
+#include <optional>
+#include <string_view>
+
+
+namespace util {
+// No bounds check! Make sure it fits within `T_uint`.
+template<typename T_uint> std::optional<T_uint> sv_to_uint(std::string_view sv)
+{
+  T_uint value{};
+
+  if (std::from_chars(sv.data(), sv.data() + sv.size(), value).ec == std::errc{}) {
+    return value;
+  } else {
+    return std::nullopt;
+  }
+}
+}// namespace util
