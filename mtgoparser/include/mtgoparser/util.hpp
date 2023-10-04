700a24e958c5b3bcd76e9e40e2ff26bcc5d50634
CramBL <marcbkonig@gmail.com>
2023-10-04 21:30:15 +0200

    add utility for string_view comparison

--- mtgoparser/include/mtgoparser/util.hpp
+++ mtgoparser/include/mtgoparser/util.hpp
@@ -5,8 +5,29 @@
 
 
 namespace util {
+
+template<typename T> [[nodiscard]] inline constexpr auto as_sv(T t) -> std::string_view
+{
+  return static_cast<std::string_view>(t);
+}
+
+template<typename SA, typename SB>
+  requires std::convertible_to<SA, std::string_view> && std::convertible_to<SB, std::string_view>
+[[nodiscard]] inline constexpr auto is_sv_same(SA a, SB b) -> bool
+{
+  return as_sv(a) == as_sv(b);
+}
+
+template<typename SA, typename... Ss>
+  requires std::convertible_to<SA, std::string_view> && (std::convertible_to<Ss, std::string_view> || ...)
+[[nodiscard]] inline constexpr auto is_sv_any_of(SA a, Ss... bs) -> bool
+{
+  return ((as_sv(a) == as_sv(bs)) || ...);
+}
+
+
 // No bounds check! Make sure it fits within `T_uint`.
-template<typename T_uint> std::optional<T_uint> sv_to_uint(std::string_view sv)
+template<typename T_uint> [[nodiscard]] inline auto sv_to_uint(std::string_view sv) -> std::optional<T_uint>
 {
   T_uint value{};
 
