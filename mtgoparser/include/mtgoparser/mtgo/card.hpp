c6061044b356487e1999b57d9be47fc605775927
CramBL <marcbkonig@gmail.com>
2023-10-04 15:47:40 +0200

    change id to uint32

--- mtgoparser/include/mtgoparser/mtgo/card.hpp
+++ mtgoparser/include/mtgoparser/mtgo/card.hpp
@@ -74,8 +74,8 @@ struct Card
     bool foil = false,
     double goatbots_price = 0,
     std::optional<double> scryfall_price = {}) noexcept
-    : id_{ id }, quantity_{ static_cast<uint16_t>(quantity) }, name_{ name }, set_{ set }, rarity_{ rarity },
-      foil_{ foil }, goatbots_price_{ goatbots_price }, scryfall_price_{ scryfall_price }
+    : id_{ static_cast<uint32_t>(id) }, quantity_{ static_cast<uint16_t>(quantity) }, name_{ name }, set_{ set },
+      rarity_{ rarity }, foil_{ foil }, goatbots_price_{ goatbots_price }, scryfall_price_{ scryfall_price }
   {}
 
   // Move constructor
