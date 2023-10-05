36c1f694715f7cc2ae4ece7a45fe5fd444aa39e0
CramBL <marcbkonig@gmail.com>
2023-10-05 13:49:41 +0200

    add sort buttons

--- mtgogui/src/main.rs
+++ mtgogui/src/main.rs
@@ -23,6 +23,7 @@ mod util;
 use menubar::McmMenuBar;
 use mtgoupdater::mtgo_preprocessor_api::run_mtgo_preprocessor_version;
 use mtgoupdater::mtgogetter_api::mtgogetter_version;
+use table::{Category, CtMessage};
 
 use crate::util::center;
 
@@ -32,11 +33,14 @@ const DEFAULT_APP_WIDTH: i32 = 1400;
 const DEFAULT_APP_HEIGHT: i32 = 800;
 const WIDGET_PADDING: i32 = 0;
 
+const TABLE_WIDTH: i32 = 790;
+
 #[derive(Debug, Clone, Copy)]
 enum Message {
     Quit,
     Example,
     MenuBar(menubar::MbMessage),
+    Table(table::CtMessage),
 }
 
 impl From<menubar::MbMessage> for Message {
@@ -72,6 +76,7 @@ impl MtgoGui {
 
         main_win.set_color(Color::Black);
         let menu = McmMenuBar::new(DEFAULT_APP_WIDTH, 25, &ev_send);
+
         let mut flx_left_col = Flex::default().with_pos(0, 35).with_size(400, 600).column();
         flx_left_col.set_align(enums::Align::LeftTop);
         let mut btn_example = button::Button::new(0, 0, 100, 25, "Example");
@@ -83,13 +88,34 @@ impl MtgoGui {
                 b.set_label("Getting example...");
             }
         });
+
         flx_left_col.end();
         let mut flex_right_col = Flex::default()
             .with_pos(400, 35)
             .with_size(1000, 600)
             .column();
         flex_right_col.set_align(enums::Align::LeftTop);
-        let collection_table = table::CollectionTable::default();
+
+        let mut flx_top_table = Flex::default()
+            .with_pos(0, 0)
+            .with_size(TABLE_WIDTH, 0)
+            .row();
+        flx_top_table.set_align(enums::Align::RightTop);
+        let mut btn_sort_quantity = button::Button::new(0, 0, 0, 0, "Sort Quantity");
+        btn_sort_quantity.emit(
+            ev_send,
+            Message::Table(CtMessage::SortBy(Category::Quantity)),
+        );
+        let mut btn_sort_name = button::Button::new(0, 0, 0, 0, "Sort Name");
+        btn_sort_name.emit(ev_send, Message::Table(CtMessage::SortBy(Category::Name)));
+
+        flx_top_table.fixed(&btn_sort_quantity, 100);
+        flx_top_table.fixed(&btn_sort_name, 100);
+        flx_top_table.end();
+
+        flex_right_col.fixed(&flx_top_table, 50);
+        let collection_table = table::CollectionTable::new(TABLE_WIDTH, 720);
+        flex_right_col.end();
 
         main_win.end();
         main_win.show();
@@ -120,27 +146,9 @@ impl MtgoGui {
                     Message::Example => {
                         let cards: Vec<mtgoupdater::mtgo_card::MtgoCard> =
                             mtgoupdater::internal_only::get_example_card_collection();
-                        cards.into_iter().for_each(|c| {
-                            self.collection.table.append_row(
-                                "",
-                                &[
-                                    &c.name,
-                                    &c.quantity.to_string(),
-                                    if c.foil { "Yes" } else { "No" },
-                                    &format!("{:8.3}", c.goatbots_price),
-                                    &{
-                                        if let Some(p) = c.scryfall_price {
-                                            p.to_string()
-                                        } else {
-                                            "N/A".into()
-                                        }
-                                    },
-                                    &c.set,
-                                    &c.rarity,
-                                ],
-                            );
-                        });
+                        self.collection.set_cards(cards);
                     }
+                    Message::Table(t_m) => self.collection.handle_ev(t_m),
                 }
             }
         }
