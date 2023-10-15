cd1ae5e50bf06f6f20bf7f17a53415a5e45b15a1
Marc Konig <mbkj@tutamail.com>
2023-10-15 22:29:47 +0200

    seperate setup step

---
+++ mtgoparser/include/mtgo_preprocessor/setup.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace mtgo_preprocessor::setup {
+
+/// Initiate logger and config
+///
+/// returns 0 on success
+int setup(int argc, char *argv[]);
+
+
+}// namespace mtgo_preprocessor::setup
---
+++ mtgoparser/src/mtgo_preprocessor/setup.cpp
@@ -0,0 +1,23 @@
+#include "mtgo_preprocessor/setup.hpp"
+#include <mtgoparser/clap.hpp>
+#include <mtgoparser/config.hpp>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
+namespace mtgo_preprocessor::setup {
+
+int setup(int argc, char *argv[])
+{
+  // https://github.com/gabime/spdlog/wiki/0.-FAQ#switch-the-default-logger-to-stderr
+  spdlog::set_default_logger(spdlog::stderr_color_st("rename_default_logger_to_keep_format"));
+  spdlog::set_default_logger(spdlog::stderr_color_st(""));
+
+  // Parse (and validate) command-line arguments
+  if (auto errors = config::Config::get()->Parse(argc, argv)) {
+    spdlog::error("{} arguments failed to validate", errors);
+    return -1;
+  };
+  return 0;
+}
+
+}// namespace mtgo_preprocessor::setup
