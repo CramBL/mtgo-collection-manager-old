35931fe2dd1fcc9f3b5ba0eabd7a3fa861f776db
Clang Robot <robot@example.com>
2023-10-21 14:15:24 +0000

    :art: Committing clang-format changes

--- mtgoparser/include/mtgoparser/util.hpp
+++ mtgoparser/include/mtgoparser/util.hpp
@@ -9,7 +9,7 @@
 namespace util {
 
 template<typename SA, typename SB>
-  requires std::convertible_to<SA, std::string_view> && std::convertible_to<SB, std::string_view>
+requires std::convertible_to<SA, std::string_view> && std::convertible_to<SB, std::string_view>
 [[nodiscard]] inline constexpr auto is_sv_same(SA a_sv, SB b_sv) -> bool
 {
   return boost::implicit_cast<std::string_view>(a_sv) == boost::implicit_cast<std::string_view>(b_sv);
@@ -17,8 +17,8 @@ template<typename SA, typename SB>
 }
 
 template<typename SA, typename... Ss>
-  requires std::convertible_to<SA, std::string_view> && (std::convertible_to<Ss, std::string_view> || ...)
-[[nodiscard]] inline constexpr auto is_sv_any_of(SA a_sv, Ss... bs_svs) -> bool
+requires std::convertible_to<SA, std::string_view> &&(std::convertible_to<Ss, std::string_view> || ...)
+  [[nodiscard]] inline constexpr auto is_sv_any_of(SA a_sv, Ss... bs_svs) -> bool
 {
   return (is_sv_same(a_sv, bs_svs) || ...);
 }
