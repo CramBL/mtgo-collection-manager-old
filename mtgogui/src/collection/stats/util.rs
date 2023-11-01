9bf65007d060c0521d9e5973259969c128f5dfd9
CramBL <marcbkonig@gmail.com>
2023-11-01 12:37:32 +0100

    multiline stat utility struct

--- mtgogui/src/collection/stats/util.rs
+++ mtgogui/src/collection/stats/util.rs
@@ -17,3 +17,34 @@ impl UniqueTotal {
         self.total
     }
 }
+
+#[derive(Debug, Clone, Default)]
+pub struct MultiValueStat {
+    title: String,
+    values: Option<Vec<String>>,
+}
+
+impl MultiValueStat {
+    pub fn new(title: String, values: Vec<String>) -> Self {
+        Self {
+            title,
+            values: Some(values),
+        }
+    }
+
+    pub fn add_value(&mut self, value: String) {
+        if let Some(values) = &mut self.values {
+            values.push(value);
+        } else {
+            self.values = Some(vec![value]);
+        }
+    }
+
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub fn take_values(&mut self) -> Vec<String> {
+        self.values.take().unwrap_or_default()
+    }
+}
