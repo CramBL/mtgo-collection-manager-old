ee30cea7488b5f862857f8c0e0e3ea6dd8e161ee
CramBL <marcbkonig@gmail.com>
2023-11-03 15:51:18 +0100

    add helper category stat item

--- mtgogui/src/collection/stats/util.rs
+++ mtgogui/src/collection/stats/util.rs
@@ -48,3 +48,34 @@ impl MultiValueStat {
         self.values.take().unwrap_or_default()
     }
 }
+
+#[derive(Debug, Clone, Default)]
+pub struct CategoryStat {
+    title: String,
+    value_pairs: Option<Vec<(String, String)>>,
+}
+
+impl CategoryStat {
+    pub fn new(title: String, value_pairs: Vec<(String, String)>) -> Self {
+        Self {
+            title,
+            value_pairs: Some(value_pairs),
+        }
+    }
+
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub fn take_value_pairs(&mut self) -> Vec<(String, String)> {
+        self.value_pairs.take().unwrap_or_default()
+    }
+
+    pub fn add_value_pair(&mut self, description: String, value: String) {
+        if let Some(value_pairs) = &mut self.value_pairs {
+            value_pairs.push((description, value));
+        } else {
+            self.value_pairs = Some(vec![(description, value)]);
+        }
+    }
+}
