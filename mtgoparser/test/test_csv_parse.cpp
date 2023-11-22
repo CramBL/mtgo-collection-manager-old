0eecc236cd2c99a5f61a8c4c0baaa2c544e64a70
CramBL <mbkj@tutamail.com>
2023-11-22 22:06:45 +0100

    remove invalid trailing '.0' in test data

--- mtgoparser/test/test_csv_parse.cpp
+++ mtgoparser/test/test_csv_parse.cpp
@@ -21,16 +21,16 @@ TEST_CASE("mtgo::csv::into_substr_vec")
   const std::string test_csv_data =
     R"(id,quantity,name,set,rarity,foil,2023-11-06T083944Z,2023-11-06T115147Z,2023-11-08T084732Z
 120020,1,In the Darkness Bind Them,LTC,R,false,[4]0.72;0.1,[8]0.78;-,0.4;0.3
-106729,1,Razorverge Thicket,ONE,R,false,[1]1.1;0.9,2.0;2.1,[11]0.9;-
-106729,1,Razorverge Thicket,THR,R,false,-;-,[2]2.0;2.1,[0]0.9;-)";
+106729,1,Razorverge Thicket,ONE,R,false,[1]1.1;0.9,2;2.1,[11]0.9;-
+106729,1,Razorverge Thicket,THR,R,false,-;-,[2]2;2.1,[0]0.9;-)";
 
   std::vector<std::string> rows = mtgo::csv::into_substr_vec(test_csv_data, '\n');
   REQUIRE(rows.size() == 4);
 
   CHECK(rows.at(0) == "id,quantity,name,set,rarity,foil,2023-11-06T083944Z,2023-11-06T115147Z,2023-11-08T084732Z");
   CHECK(rows.at(1) == "120020,1,In the Darkness Bind Them,LTC,R,false,[4]0.72;0.1,[8]0.78;-,0.4;0.3");
-  CHECK(rows.at(2) == "106729,1,Razorverge Thicket,ONE,R,false,[1]1.1;0.9,2.0;2.1,[11]0.9;-");
-  CHECK(rows.at(3) == "106729,1,Razorverge Thicket,THR,R,false,-;-,[2]2.0;2.1,[0]0.9;-");
+  CHECK(rows.at(2) == "106729,1,Razorverge Thicket,ONE,R,false,[1]1.1;0.9,2;2.1,[11]0.9;-");
+  CHECK(rows.at(3) == "106729,1,Razorverge Thicket,THR,R,false,-;-,[2]2;2.1,[0]0.9;-");
 
 
   auto headers = mtgo::csv::into_substr_vec(rows[0], ',');
@@ -118,8 +118,8 @@ TEST_CASE("mtgo::csv::into_substr_vec & mtgo::csv::str_to_floats")
   const std::string test_csv_data =
     R"(id,quantity,name,set,rarity,foil,2023-11-06T083944Z,2023-11-06T115147Z,2023-11-08T084732Z
 120020,1,In the Darkness Bind Them,LTC,R,false,[4]0.72;0.1,[8]0.78;-,0.4;0.3
-106729,1,Razorverge Thicket,ONE,R,false,[1]1.1;0.9,2.0;2.1,[11]0.9;-
-106729,1,Razorverge Thicket,THR,R,false,-;-,[2]2.0;2.1,[0]0.9;-)";
+106729,1,Razorverge Thicket,ONE,R,false,[1]1.1;0.9,2;2.1,[11]0.9;-
+106729,1,Razorverge Thicket,THR,R,false,-;-,[2]2;2.1,[0]0.9;-)";
 
   std::vector<std::string> rows = mtgo::csv::into_substr_vec(test_csv_data, '\n');
   REQUIRE(rows.size() == 4);
@@ -210,8 +210,8 @@ TEST_CASE("mtgo::csv::floats_from_span")
     const std::string test_csv_data =
       R"(id,quantity,name,set,rarity,foil,2023-11-06T083944Z,2023-11-06T115147Z,2023-11-08T084732Z
 120020,1,In the Darkness Bind Them,LTC,R,false,[4]0.72;0.1,[8]0.78;-,0.4;0.3
-106729,1,Razorverge Thicket,ONE,R,false,[1]1.1;0.9,2.0;2.1,[11]0.9;-
-106729,1,Razorverge Thicket,THR,R,false,-;-,[2]2.0;2.1,[0]0.9;-)";
+106729,1,Razorverge Thicket,ONE,R,false,[1]1.1;0.9,2;2.1,[11]0.9;-
+106729,1,Razorverge Thicket,THR,R,false,-;-,[2]2;2.1,[0]0.9;-)";
 
     std::vector<std::string> rows = mtgo::csv::into_substr_vec(test_csv_data, '\n');
     REQUIRE(rows.size() == 4);
